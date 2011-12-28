48e012f014595a038bbbdc622870b741bd86aa1c
Argan Wang <argan_w@yahoo.com.cn>
2011-12-28 10:54:18 +0800

    remove branches/tags/trunk/wiki

--- branches/pxb/src/main/java/org/hydra/renamer/item/FieldInfo.java
+++
@@ -1,7 +0,0 @@
-package org.hydra.renamer.item;
-
-public class FieldInfo extends MemberInfo {
-
-	public Object value;
-	// public ClassInfo type;
-}
--- trunk/src/main/java/org/hydra/renamer/item/MethodInfo.java
+++
@@ -1,7 +0,0 @@
-package org.hydra.renamer.item;
-
-
-public class MethodInfo extends MemberInfo{
-//	public ClassInfo ret;
-//	public List<ClassInfo> args = new ArrayList<ClassInfo>();
-}
